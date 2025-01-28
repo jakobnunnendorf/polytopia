061d5db796e2a50ad42e74e036d4ddce6b071a57
Jakob <jnunnendorf@icloud.com>
2025-01-28 18:01:53 +0800

    style(index.tsx): add background

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,23 @@
+import { View, Image } from "react-native";
 import Map from "@/components/Map/Map";
-import { View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View className="h-screen grid place-content-center">
-      <Map />
+    <View className="flex-1">
+      {/* Background Image */}
+      <Image
+        source={require("@/assets/images/menu-background.webp")}
+        className="absolute top-0 left-0 w-full h-full"
+        resizeMode="cover"
+      />
+
+      {/* Map Container */}
+      <View className="h-screen grid place-content-center">
+        <div className="px-32 pb-16 bg-white/90 rounded-3xl font-family-[Oi-Regular]">
+          <h1 className="text-3xl font-bold py-8 text-center">Polytopia</h1>
+          <Map />
+        </div>
+      </View>
     </View>
   );
 }
